a2e8652c32c6adbb1651279cd92df93f124a6a63
Hayden Cheers <hayden.cheers@me.com>
2020-04-15 11:02:18 +1000

    sim wine filewise optimisation

--- sim/src/main/kotlin/me/haydencheers/scpdt/sim/SimWineSCPDTool.kt
+++ sim/src/main/kotlin/me/haydencheers/scpdt/sim/SimWineSCPDTool.kt
@@ -86,6 +86,112 @@ class SimWineSCPDTool: SCPDTool {
         }
     }
 
+    override fun evaluateAllFiles(ldir: Path, rdir: Path): List<Triple<String, String, Double>> {
+        if (!::simPath.isInitialized) throw IllegalStateException("Field simPath is not thawed")
+
+        TempUtil.copyPairwiseInputsToTempDirectory(ldir, rdir).use { (tmp, lhs, rhs) ->
+            val lJavaFiles = Files.walk(lhs)
+                .filter { Files.isRegularFile(it) && it.fileName.toString().endsWith(".java") }
+                .use { it.toList() }
+                .map { it.toAbsolutePath().toString() }
+
+            val rJavaFiles = Files.walk(rhs)
+                .filter { Files.isRegularFile(it) && it.fileName.toString().endsWith(".java") }
+                .use { it.toList() }
+                .map { it.toAbsolutePath().toString() }
+
+            if (lJavaFiles.isEmpty() || rJavaFiles.isEmpty()) return emptyList()
+
+            val proc = ProcessBuilder()
+                .command(
+                    wineStr,
+                    simPathStr,
+                    "-R",
+                    "-s",
+                    "-p",
+                    lhs.toAbsolutePath().toString(),
+                    rhs.toAbsolutePath().toString()
+                ).start()
+
+            proc.waitFor()
+
+            val output = proc.inputStream.bufferedReader().use { it.readLines() }
+
+            if (proc.exitValue() != 0) {
+                val err = proc.errorStream.bufferedReader().use { it.readLine() }
+                throw IllegalStateException("Received error code ${proc.exitValue()}")
+            }
+
+            val lFiles = mutableListOf<String>()
+            val rFiles = mutableListOf<String>()
+
+            var i = 0
+
+            val rootDir = tmp.toAbsolutePath().toString()
+            while (i < output.size) {
+                val line = output[i]
+
+                if (line.startsWith("Total input:")) {
+                    val lineNoPrefix = line.removePrefix("Total input: ")
+                    val afterNumberIndex = lineNoPrefix.indexOf(" files (")
+                    val number = lineNoPrefix.substring(0, afterNumberIndex)
+
+                } else if (line == "") {
+                    i++
+                    break
+
+                } else {
+                    val file = line.removePrefix("File ")
+                        .split(": ")[0]
+                        .removePrefix(rootDir)
+                        .removePrefix(File.separator)
+
+                    if (file.startsWith("lhs") && file.endsWith(".java"))
+                        lFiles.add(file)
+                    else if (file.startsWith("rhs") && file.endsWith(".java"))
+                        rFiles.add(file)
+                }
+
+                i++
+            }
+
+            if (lFiles.isEmpty() || rFiles.isEmpty()) return emptyList()
+
+            val sims = mutableListOf<Triple<String, String, Double>>()
+            while (i < output.size) {
+                val line = output[i]
+
+                val component1 = line.split(" consists for ")
+                val component2 = component1[1].split(" % of ")
+
+                val sim = component2[0].toDouble()
+                val lFile = component1[0].removePrefix(rootDir).removePrefix(File.separator)
+                val rFile = component2[1].removeSuffix(" material").removePrefix(rootDir).removePrefix(File.separator)
+
+                if (lFile.startsWith("lhs") && rFile.startsWith("rhs")) {
+                    val lIndex = lFiles.indexOf(lFile)
+                    val rIndex = rFiles.indexOf(rFile)
+
+                    if (lIndex != -1 && rIndex != -1) {
+                        sims.add(Triple(lFile.removePrefix("lhs/"), rFile.removePrefix("rhs/"), sim))
+                    }
+
+                } else if (rFile.startsWith("lhs") && lFile.startsWith("rhs")) {
+                    val lIndex = rFiles.indexOf(rFile)
+                    val rIndex = lFiles.indexOf(lFile)
+
+                    if (lIndex != -1 && rIndex != -1) {
+                        sims.add(Triple(rFile.removePrefix("lhs/"), lFile.removePrefix("rhs/"), sim))
+                    }
+                }
+
+                i++
+            }
+
+            return sims
+        }
+    }
+
     private fun calculateSimilarity(output: List<String>, rootDir: String): Double {
         val lFiles = mutableListOf<String>()
         val rFiles = mutableListOf<String>()
@@ -177,4 +283,28 @@ class SimWineSCPDTool: SCPDTool {
 
         return sim
     }
+}
+
+fun main() {
+    val tool = SimWineSCPDTool()
+    tool.thaw()
+
+    val root = Paths.get("/media/haydencheers/Data/PrEP/datasets/COMP2240_2018_A1")
+
+    val dirs = Files.list(root)
+        .filter { Files.isDirectory(it) && !Files.isHidden(it) }
+        .toList()
+
+    for (l in 0 until dirs.size) {
+        val ldir = dirs[l]
+
+        for (r in l+1 until dirs.size) {
+            val rdir = dirs[r]
+
+            val res = tool.evaluateAllFiles(ldir, rdir)
+        }
+    }
+
+    println("Done")
+    tool.close()
 }
